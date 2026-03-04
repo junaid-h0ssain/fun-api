fa5ed940c6738f87dc5199168490d9dc2e89c430
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-03-04 07:54:18 +0600

    response object way works

--- exp.py
+++ exp.py
@@ -3,3 +3,10 @@ from main import FunAPI
 
 funapi = FunAPI()
 
+@funapi.get("/users")
+def get_users(req, res):
+    res['status_code'] = "200 OK"
+    res['headers'] = []
+    res['text'] = "['my','name','is','funapi']"
+
+
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 
+
+
+
 class FunAPI:
     def __init__(self):
-        pass
+        self.routes = dict()
 
     def __call__(self, environ, start_response):
-        print(environ)
-        start_response("200 OK", headers=[])
-        return[b"Hello, World."]
-    
+        response = {}
+        for path, handlerDict in self.routes.items():
+            for requestMethod, handler in handlerDict.items():
+                if path == environ["PATH_INFO"] and requestMethod == environ["REQUEST_METHOD"]:
+                    handler(environ, response)
+                    start_response(response['status_code'], headers=response['headers'])
+                    return [response['text'].encode()]
+                
 
 
-funapi = FunAPI()
+    def get(self, path=None):
+        def wrapper(handler):
+            pathName = path or f"/{handler.__name__}"
+
+            if pathName not in self.routes: 
+                self.routes[pathName] = {}
+
+            self.routes[pathName]["GET"] = handler
+            
+            print(self.routes)
+
+        return wrapper
