b9a0521b66b3ad6424e842d60fac039facd6915c
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-03-04 06:11:26 +0600

    basic setup complete

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def app(environ, start_response):
-    print(environ)
-    start_response("200 OK", headers=[])
-    return [b"Hello World"]
+
+class FunAPI:
+    def __init__(self):
+        pass
+
+    def __call__(self, environ, start_response):
+        print(environ)
+        start_response("200 OK", headers=[])
+        return[b"Hello, World."]
+    
+
+
+funapi = FunAPI()
